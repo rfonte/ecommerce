dbb48652a582a85842b2500c99ca10209c86bba4
Rodrigo - Note <rodrigo.fonte@yahoo.com.br>
2010-07-26 22:28:18 -0300

    iniciada a implementação do css e js do login

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,10 +1,10 @@
 $(document).ready (function() {
     $(".textbox").textboxClear()
 
-    $("#pwd").click(function() {
+    /*$("#pwd").click(function() {
         $(this)
             .attr("value","")
             document.getElementById("pwd").setAttribute("type","password");
-    });
+    });*/
 });
 
---
+++ public/javascripts/login.js
@@ -0,0 +1,24 @@
+$(document).ready (function() {
+
+    $("#loginLink").click(function(){
+        $('#login')
+            .css("display","block")
+        $('.textbox')
+            .eq(0).focus()
+    }),
+
+    $(".loginbtn").click(function() {
+        $('#login')
+            .css("display","block")
+    }),
+    /*
+    $(".logout").click(function() {
+
+    }),*/
+
+    $(".close_login").click(function() {
+        $('#login')
+            .css("display","none")
+    })
+});
+
