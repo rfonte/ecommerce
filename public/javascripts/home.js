840fc746c395b34c004d61f444e98379cda49a7b
Rodrigo Fonte <rodrigo.fonte@yahoo.com.br>
2010-08-12 15:17:19 -0700

    implementado o login na home

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,48 @@
 $(document).ready (function() {
-    $(".textbox").textboxClear()
+    $(".textbox").textboxClear();
+
+    $("#loginLink").click(function() {
+        if ($("#divLogin").css("display") === "block"){
+            $("#divLogin").hide()
+        }
+        else {
+            $("#divLogin").show()
+        }
+    })
+
+    /*$(this).click (function() {
+        if ($("#divLogin").click()){
+            $("#divLogin").show();
+        } else {
+            $("#divLogin").hide();
+        }
+    })*/
+
+   /*
+   Solução para chamar outra página de seção
+    $("#loginLink").click(function(event) {
+        event.preventDefault();
+        var html_login = "";
+        $.ajax({
+            url: $(this).attr("href"),
+
+            success: function(data) {
+                html_login = $(data);
+                html_login.show();
+            },
+
+            error: function () {
+                loading.set.message("Erro de conexão");
+            },
+
+            complete: function(xhr, ts) {
+                if (xhr.status === 200) {
+                   // home.oficina.render(html_login);
+                }
+            },
+            dataType:"html"
+        });
+    });*/
 
     /*$("#pwd").click(function() {
         $(this)
